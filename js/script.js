17ab8a49df49c0dd7284d9961bd1f8947992e40c
SUBRATA ROY <subrata.r321@gmail.com>
2020-07-23 10:39:19 -0400

    [HTML] html page created using JS

--- js/script.js
+++ js/script.js
@@ -1,49 +1,36 @@
 const body = document.querySelector('body');
 
 document.addEventListener('DOMContentLoaded', (event) => {
-    //console.log('DOM loaded');
-
-    // <div class="container">
-    // <div class= image>
-    //   <img src="https://3.bp.blogspot.com/-Nuq97fie9Y0/WbzZuNG5OBI/AAAAAAAKRp8/V-CkGZB17M4DPZtcKyHgVIYGcZDC1lLSwCLcBGAs/s1600/AW540451_01.gif" alt="Panda" >
-    //   </br> </br>
-    // </div>
-
-    // <div class="search">
-    //   <h1>Panda Image Search</h1>
-    //   <input type="search"></input>
-    //   </br> </br>
-    //   <Button type="search"> 🔍  Search</Button>
-    // </div>
+    
 
     const container = document.createElement('div');
     container.classList.add('container');
     container.style.textAlign = 'center';
     container.style.color = ' #f5f5f5;';
+    body.appendChild(container);
     
     const imageLogoDiv = document.createElement('div');
     imageLogoDiv.classList.add('image');
+    container.appendChild(imageLogoDiv);
     
     const imageLogo = document.createElement('img');
     imageLogo.src = 'https://3.bp.blogspot.com/-Nuq97fie9Y0/WbzZuNG5OBI/AAAAAAAKRp8/V-CkGZB17M4DPZtcKyHgVIYGcZDC1lLSwCLcBGAs/s1600/AW540451_01.gif';
     imageLogo.alt = 'Panda';
     //console.log(imageLOgo);
     imageLogoDiv.appendChild(imageLogo);
-    container.appendChild(imageLogoDiv);
-    body.appendChild(container);
+    imageLogoDiv.appendChild(document.createElement('br'));
     
-    
-    const newLine = imageLogoDiv.innerHTML= '</br> </br>';
+    // const newLine = imageLogoDiv.innerHTML= '</br> </br>';
 
-    const search = document.createElement('div');
-    search.classList.add('search');
+    const searchDiv = document.createElement('div');
+    searchDiv.classList.add('search');
     
     const header = document.createElement('h1');
     header.textContent = 'Panda Image Search';
     
     const input = document.createElement('input');
     input.type = 'Search';
-    input.height= '40px';
+    input.height= '36px';
     input.width= '450px';
     input.border= '1px';
     
@@ -54,11 +41,12 @@ document.addEventListener('DOMContentLoaded', (event) => {
     button.style.height= '36px';
     button.style.width= '100px';
     
-    search.appendChild(header);
-    search.appendChild(input);
-    search.appendChild(button);
-    Image.innerHTML= '</br> </br>';
-    body.appendChild(search);
+    searchDiv.appendChild(header);
+    searchDiv.appendChild(input);
+    searchDiv.appendChild(document.createElement('br'));
+    searchDiv.appendChild(document.createElement('br'));
+    searchDiv.appendChild(button);
+    container.appendChild(searchDiv);
 
 
     
